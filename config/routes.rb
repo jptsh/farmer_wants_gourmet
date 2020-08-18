ffb9d425798bc1b4de75f71b8378fe284267616e
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-18 12:57:53 +0200

    Some update on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   root to: 'pages#home'
 
-  resources :boxes, do
-    resources :orders, except: [ :edit, :update]
+  resources :boxes do
+    resources :orders, except: [ :edit, :update ]
   end
 
   #resources :boxes, only: [ :destroy ]
