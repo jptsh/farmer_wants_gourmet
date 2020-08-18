12a84b5d6d20e3a02441da08f641636fbc5a7592
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-18 16:04:53 +0200

    Seed-file added

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,6 +1,5 @@
 class BoxesController < ApplicationController
-  before_action :set_box, only [:show, :destroy, :update ]
-  
+  before_action :set_box
   def index
     @boxes = Box.all
   end
@@ -16,7 +15,7 @@ class BoxesController < ApplicationController
   def create
     @box = Box.new(box_params)
     if @box.save
-      redirect_to @cocktail, notice: 'Box was successfully created.'
+      redirect_to @box, notice: 'Box was successfully created.'
     else
       render :new
     end
