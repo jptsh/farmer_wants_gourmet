fb29fa43d55209c46bf1bb187343b7670edd53b2
Jaber AlAdawi <jaberadawi@gmail.com>
2020-08-18 18:34:45 +0200

    ordercontroller and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,20 @@
 class OrdersController < ApplicationController
+
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_box, only: [:new, :create]
+
+
   def new
+    @box = Box.find(params[:box_id])
     @order = Order.new
   end
 
   def create
-
+    @box = Box.find(params[:box_id])
     @order = Order.new(order_params)
     @order.box = @box
-    @order.user = @user 
+    @order.user = @user
+    @order.total_price = @box.price
     @order.save!
 
     redirect_to user_path(@home)
@@ -16,8 +23,16 @@ class OrdersController < ApplicationController
   end
 
   private
+    
+    def set_order
+      @order = Order.find(params[:id])
+    end
+    def set_box
+      @box = Box.find(params[:box_id])
+    end
 
-  def order_params
-    params.require(:order).permit(:total_price)
-  end
+
+    def order_params
+      params.require(:order).permit(:total_price)
+    end
 end
