21382a02f3f580ec81041a0344d300a298541174
Jaber AlAdawi <jaberadawi@gmail.com>
2020-08-18 15:53:48 +0200

    order controller V1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_105053) do
+ActiveRecord::Schema.define(version: 2020_08_18_130037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2020_08_18_105053) do
     t.index ["user_id"], name: "index_boxes_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "total_price"
+    t.bigint "box_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["box_id"], name: "index_orders_on_box_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +50,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_105053) do
   end
 
   add_foreign_key "boxes", "users"
+  add_foreign_key "orders", "boxes"
+  add_foreign_key "orders", "users"
 end
