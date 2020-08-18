de5f50caa884314bbcb2448c633a448a5f66f456
Zeljka Vujic <vujic.zeljka@outlook.de>
2020-08-18 17:12:33 +0200

    changes

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,12 +1,12 @@
 class BoxesController < ApplicationController
-  before_action :set_box
+  before_action :set_box, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   
   def index
     @boxes = Box.all
   end
 
   def show
-    @box = Box.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 gem 'faker'
-5.times do |i|
+5.times do
   user = User.new(
     email: Faker::Internet.email, 
     username: Faker::Internet.username, 
     password: Faker::Internet.password)
-  )
+
 end
