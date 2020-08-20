d7161e0bdc9d37ab3ee466d2a475d63cd0e0844e
Eduard Schulte <ecschulte@gmx.de>
2020-08-20 12:37:37 +0200

    Edited orders controller & its views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,50 @@
 class OrdersController < ApplicationController
 
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
-  before_action :set_box, only: [:new, :create]
+  def index
+    @orders_of_user = policy_scope(Order).where(:user == current_user)
 
+    @incomingorder = []
+    @orderrequest = []
+
+    @orders_of_user.each do |order|
+      if order.box.user == current_user
+        @incomingorder << order
+      else
+        @orderrequest << booking
+      end
+    end
+  end
 
   def new
     @box = Box.find(params[:box_id])
     @order = Order.new
+    authorize @order
   end
 
   def create
     @box = Box.find(params[:box_id])
     @order = Order.new(order_params)
+    @order.user = current_user
     @order.box = @box
-    @order.user = @user
-    @order.total_price = @box.price
-    @order.save!
+    authorize @order
 
-    redirect_to user_path(@home)
-
-    # render "order/show"
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
   end
 
-  private
-    
-    def set_order
-      @order = Order.find(params[:id])
-    end
-    def set_box
-      @box = Box.find(params[:box_id])
-    end
+  def show
+    @order = Order.find(params[:id])
+    authorize @order
+  end
 
+  def edit
+  end
 
-    def order_params
-      params.require(:order).permit(:total_price)
-    end
+  def order_params
+    params.permit(:user_id, :box_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   #resources :boxes, only: [ :destroy ]
-  #resources :orders, only: [ :destroy ]
+  resources :orders
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
